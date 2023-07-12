69522ce1f52bd2cc85f85da7cbb5111133260738
Nicholas Tang <nicholas.tang@knocaminolta.com>
2023-07-12 12:19:33 +0800

    added timeout for the waiting in case of indefinite

--- terraform/kubernetes/scripts/wait.sh
+++ terraform/kubernetes/scripts/wait.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $CURRENT_DIR
+count=0
 while [ ! -f "done.txt" ]; do
   echo "Waiting for runcmd to finish"
   sleep 5
+  count = $((count+1))
+  if [ $count -gt 30 ]; then
+    echo "Timeout waiting for runcmd to finish"
+    exit 1
+  fi
 done
